a2839a0007b2a47d11065ced6ac3f5f324dde9d2
Viorel Rinja <viorel.rinja@gmail.com>
2019-11-28 22:17:17 +0200

    rethink mouse movement model, implement separation between dragging and moving

--- src/ui/mouse.js
+++ src/ui/mouse.js
@@ -1,67 +1,81 @@
+const PRIMARY = 'primary';
+const SECONDARY = 'secondary';
+const MIDDLE = 'middle';
+
+/*
+
+    The module exports only the instance of Mouse class.
+    It listens to all mouse events and processes them.
+    For now, at the start of the program, we should bind the html node to the instance with bindObject().
+
+    Create an event listener is initiated with _initEventListener().
+    It should be called only once for every event type.
+    Should be called with delegates which is either a function, or an array of functions.
+    The delegates item/s will be executed for every fire of the event.
+
+*/
+
 class Mouse {
     constructor() {
         this.obj = null;
-        this.mouseMoveDelegates = [];
-        this.mouseDownDelegates = [];
     }
 
     bindObject(object) {
         this.obj = object;
     }
 
-    initListeners(){
-        this._preventContextMenu();
-        this.initMouseMove();
-        this.initMouseDown();
-    }
-
     _preventContextMenu() {
         this.obj.addEventListener('contextmenu', (e) => e.preventDefault())
     }
 
-    _initEventListener(event, delegates, payload) {
+    _getMousePosition(e) {
+        return {
+            'x': e.clientX,
+            'y': e.clientY
+        }
+    }
+
+    _getMouseClick(e) {
+        if (e.which === 1) {
+            return PRIMARY;
+        } else if (e.which === 3) {
+            return SECONDARY;
+        } else if (e.which === 2) {
+            return MIDDLE
+        }
+
+        return false;
+    }
+
+    _initEventListener(event, delegates) {
         const self = this;
         self.obj.addEventListener(event, function (e) {
-            delegates.map(item => item(payload, e))
+            if (Array.isArray(delegates)) {
+                delegates.map(item => item(e))
+            } else {
+                delegates(e);
+            }
         })
     }
 
-    addMouseMoveDelegate(func) {
-        this.mouseMoveDelegates.push(func);
+    initListeners(){
+        this._preventContextMenu();
+        this.initMouseMove();
     }
 
     initMouseMove() {
-        const payload = {x: 'x', y: 'y'}
-        this._initEventListener('mousemove', this.mouseMoveDelegates, payload);
+        this._initEventListener('mousemove', this.interpretMouseMove.bind(this));
     }
 
-    initMouseDown() {
-        const payload = { x: 'x', y: 'y' }
-        const mouseDownFunctions = [];
-        
-        mouseDownFunctions.push(this._handleLeftBtnDown);
-        mouseDownFunctions.push(this._handleRightBtnDown);
-        mouseDownFunctions.push(this._handleMiddleBtnDown);
-
-        this._initEventListener('mousedown', mouseDownFunctions, payload);
-    }
-
-    _handleLeftBtnDown(payload, e) {
-        if (e.which === 1 || e.button === 0) {
-            console.log('left button down!');
-        }
-    }
-
-    _handleRightBtnDown(payload, e) {
-        if (e.which === 3 || e.button === 2) {
-            e.preventDefault();
-            console.log('right button down!')
-        }
-    }
-
-    _handleMiddleBtnDown(payload, e) {
-        if (e.which === 2 || e.button === 1) {
-            console.log('Middle button clicked!');
+    interpretMouseMove(e) {
+        if (this._getMouseClick(e) === PRIMARY) {
+            console.log('dragging primary')
+        } else if (this._getMouseClick(e) === SECONDARY) {
+            console.log('dragging secondary')
+        } else if (this._getMouseClick(e) === MIDDLE) {
+            console.log('dragging middle')
+        } else if (!!!this._getMouseClick(e)) {
+            console.log('just moving')
         }
     }
 }
